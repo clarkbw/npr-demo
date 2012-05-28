9da5ad56859e559de2a2bafe90a80f1c25b842b8
Bryan Clark <clarkbw@gmail.com>
2012-05-28 14:51:27 -0700

    first push

---
+++ server.js
@@ -0,0 +1,294 @@
+var http = require('http'),
+    URL = require('url'),
+    mime = require("mime");
+
+mime.define({
+  "text/cache-manifest" : [".appcache"],
+});
+
+var REDIS = { PORT : 6379, HOST : "127.0.0.1", PASSWORD : ""};
+
+if (process.env.VCAP_SERVICES){
+  // here's where we'd override the port and host variables
+  srv = JSON.parse(process.env.VCAP_SERVICES);
+  //console.log(srv);
+  var credentials = srv["redis-2.2"][0]["credentials"];
+  REDIS.HOST = credentials.host;
+  REDIS.PORT = credentials.port;
+  REDIS.PASSWORD = credentials.password;
+}
+
+var redis = require("redis"),
+    client = redis.createClient(REDIS.PORT, REDIS.HOST);
+
+if (process.env.VCAP_SERVICES){
+  client.auth(REDIS.PASSWORD);
+}
+
+client.on("error", function (err) {
+  console.log("Redis connection error to " + client.host + ":" + client.port + " - " + err);
+});
+
+var express = require('express'),
+    app = express.createServer();
+
+app.use(express.static(__dirname + '/www'));
+app.use(express.directory(__dirname + '/www'));
+
+app.all('/stories', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+})
+
+app.get('/stories', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.contentType('json');
+  client.hgetall("stories:info", function(err, obj) {
+    //console.log("stories:info", obj); 
+    //console.log("all, ", JSON.stringify(obj));
+    var ret = { list : { story : [] } }; 
+    for (var item in obj) {
+      //console.log(item, obj[item], JSON.parse(obj[item]));
+      ret.list[item] = JSON.parse(obj[item]);
+    }
+    client.hgetall("stories:stories", function(err, obj) {
+      var count = 10;
+      for (var item in obj) {
+        if (count-- <= 0) {
+          break;
+        }
+        ret.list.story.push(JSON.parse(obj[item]))
+      }
+      res.send(ret);
+    });
+  });
+  //res.send(stories);
+});
+
+app.get('/stories/:id', function(req, res, next) {
+  console.log('/stories/:id' + req.params.id);
+
+  res.contentType('json');
+  client.hget("stories:info", req.params.id, function(err, obj) {
+    //console.log("stories:info", obj); 
+    //console.log("all, ", JSON.stringify(obj));
+    var ret = { list : { story : [] } }; 
+    for (var item in obj) {
+      //console.log(item, obj[item], JSON.parse(obj[item]));
+      ret.list[item] = JSON.parse(obj[item]);
+    }
+    client.hget("stories:stories", req.params.id, function(err, obj) {
+      ret.list.story.push(JSON.parse(obj[item]))
+      res.send(ret);
+    });
+  });
+});
+
+app.get('/playlist', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.contentType('json');
+  client.smembers("playlist", function(err, obj) {
+    console.log("smembers:playlist", obj); 
+    //console.log("all, ", JSON.stringify(obj));
+    var ret = []; 
+    for (var item in obj) {
+      //console.log(item, obj[item], JSON.parse(obj[item]));
+      ret[item] = JSON.parse(obj[item]);
+    }
+    res.send(ret);
+  });
+
+});
+
+app.post('/playlist/:id', function(req, res, next) {
+  // Handle the post for this route
+  console.log(req.params.id);
+  client.sadd("playlist", req.params.id, function(err, obj) {
+    console.log("sadd:playlist", err, obj);
+    res.send();
+  });
+})
+
+app.post('/stories', function(req, res, next) {
+  // Handle the post for this route
+  res.send();
+})
+
+function setThumbnail(id, src, type, data) {
+  client.hget("stories:stories", id, function(err, obj) {
+    var story = JSON.parse(obj);
+    console.log(id, type);
+    if (type == "thumbnail") {
+      console.log("thumbnail", story.thumbnail.medium.$text.substr(0, 144));
+      story.thumbnail.medium.$text = data;
+      console.log("thumbnail", story.thumbnail.medium.$text.substr(0, 144));
+      client.hset("stories:stories", id, JSON.stringify(story), redis.print);
+    }
+    
+  });
+}
+function setImage(id, src, type, data) {
+  client.hget("stories:stories", id, function(err, obj) {
+    var story = JSON.parse(obj);
+    console.log(id, type);
+    for (var i = 0; story.image && i < story.image.length; i++) {
+      var image = story.image[i];
+      for (var j = 0; image.crop && j < image.crop.length; j++) {
+        if (image.crop[j].type == type && image.crop[j].src == src) {
+          console.log("crop ", type, story.image[i].crop[j].src.substr(0, 144));
+          story.image[i].crop[j].src = data;
+          console.log("crop", type, story.image[i].crop[j].src.substr(0, 144));
+        }
+      }
+    }
+    client.hset("stories:stories", id, JSON.stringify(story), redis.print);
+  });
+}
+
+function getStories() {
+  // This is a really slow hand off, we should at least cache the results
+  var options = {
+    port: 80,
+    host: 'api.npr.org',
+    path: '/query?id=1002&output=JSON&apiKey=MDA5MzY5ODQ5MDEzMzUyMTI2MDY1NGI2Zg001',
+    method: 'GET'
+  };
+  var jsonresponse = "", request;
+  try {
+  request = http.request(options);
+  request.on('response', function (response) {
+    response.on('data', function (chunk) {
+      //console.log('BODY: ' + chunk);
+      jsonresponse += chunk;
+    });
+    response.on("end", function() {
+      try {
+      var resp = JSON.parse(jsonresponse);
+      //console.log(resp);
+      for (var s in resp.list.story) {
+        var story = resp.list.story[s];
+        //console.log("story", story);
+        client.hsetnx("stories:stories", story.id, JSON.stringify(story),
+                      function(err, isNew) {
+                        var _story = resp.list.story[s]
+                        console.log("isNew", isNew);
+                        //if (isNew) {
+                        //  console.log("new", _story.thumbnail, _story.image);
+                        //  if (story.thumbnail && story.thumbnail.medium) {
+                        //    var src = story.thumbnail.medium.$text, img = URL.parse(src);
+                        //    if (img.protocol == "http:") {
+                        //      var set = function(data) { var _id = story.id, _src = src; console.log("data", _id, _src); setImage(_id, _src, "thumbnail", data); };
+                        //      console.log(story.id, src);
+                        //      getImage(src, set);
+                        //    }
+                        //  }
+                        //  for (var i = 0; story.image && i < story.image.length; i++) {
+                        //    var image = story.image[i];
+                        //    for (var j = 0; image.crop && j < image.crop.length; j++) {
+                        //      if (image.crop[j].type == "standard") {
+                        //        var src = image.crop[j].src, img = URL.parse(src);
+                        //        if (img.protocol == "http:") { 
+                        //          var set = function(data) { var _id = story.id, _src = src; console.log("data", _id, _src); setImage(_id, _src, "standard", data); };
+                        //          console.log(story.id, src);
+                        //          getImage(src, set);
+                        //        }
+                        //      }
+                        //    }
+                        //  }
+                        //}
+                      });
+      }
+      var v = ["title", "teaser", "link"];
+      for (var k in v) {
+        client.hset("stories:info", v[k], JSON.stringify(resp.list[v[k]]));
+      }
+
+      // Call update stories so we download all the images
+      updateStories();
+
+      }catch(e) { console.log("error, likely parsing", e, jsonresponse); }
+    });
+  });
+  request.end();
+  } catch(e) {
+    console.log("getStories", e);
+  }
+}
+
+function getImage(imgsrc, cb) {
+  try {
+  var img = URL.parse(imgsrc);
+  //console.log("img", img);
+  if (img) {
+    var irequest = http.createClient(80, img.hostname).request('GET', img.pathname, {'host': img.hostname});
+    irequest.on('response', function (response)
+    {
+        var type = response.headers["content-type"],
+            prefix = "data:" + type + ";base64,",
+            body = "";
+        response.setEncoding('binary');
+        response.on('end', function () {
+            var base64 = new Buffer(body, 'binary').toString('base64'),
+                data = prefix + base64;
+            //console.log("img", data);
+            cb(data);
+        });
+        response.on('data', function (chunk) {
+            if (response.statusCode == 200) body += chunk;
+        });
+        response.on('error', function (e) {
+            console.log('error', e);
+        });
+    });
+    irequest.end();
+  }
+  } catch(e) { console.log("getImage", e); }
+}
+
+function updateStories() {
+  client.hgetall("stories:stories", function(err, obj) {
+    for (var item in obj) {
+      var story = JSON.parse(obj[item]);
+      console.log("updating story : ", story.id);
+      //if (story.thumbnail && story.thumbnail.medium) {
+      //   var src = story.thumbnail.medium.$text, img = URL.parse(src);
+      //    console.log("updating story : thumbnail", img.protocol);
+      //   if (img.protocol == "http:") {
+      //    var set = function(data) { var _id = story.id, _src = src; console.log("t.data", _id, _src.substr(0, 144)); setImage(_id, _src, "thumbnail", data); };
+      //    console.log(story.id, src);
+      //    getImage(src, set);
+      //   }
+      //}
+      for (var i = 0; story.image && i < story.image.length; i++) {
+        var image = story.image[i];
+        for (var j = 0; image.crop && j < image.crop.length; j++) {
+          if (image.crop[j].type == "standard" || image.crop[j].type == "square") {
+            var src = image.crop[j].src, type = image.crop[j].type, img = URL.parse(src);
+            console.log("updating story : image.crop[j].type", img.protocol);
+            if (img.protocol == "http:") {
+              var set = function(data) { var _id = story.id, _src = src, _type = type; console.log("s.data", _id, _src.substr(0, 144)); setImage(_id, _src, _type, data); };
+              console.log(story.id, type, src.substr(0, 144));
+              getImage(src, set);
+            }
+          }
+        }
+      }
+    }
+  });  
+}
+
+// Run getStories now
+getStories();
+
+updateStories();
+
+// Run getStories every hour
+//setInterval(getStories, (60 * (60 * (1 * 1000))));
+
+var port = process.env.VCAP_APP_PORT || 8080;
+console.log("listening on: ", port );
+
+app.listen(port);
---
+++ www/js/app/main.js
@@ -0,0 +1,144 @@
+// Defines the main app module. This one does the top level app wiring.
+
+define(function (require) {
+    'use strict';
+
+    var $ = require('jquery'),
+        Backbone = require('backbone'),
+        _ = require('underscore'),
+        moment = require("moment");
+
+    // Dependencies that do not have an export of their own, just attach
+    // to other objects, like jQuery. These are just used in the example
+    // bootstrap modal, not directly in the UI for the network and appCache
+    // displays.
+    require('bootstrap/transition');
+
+        _.mixin({
+          ago : function ago(date) { return moment(date).fromNow(); },
+          seconds : function seconds(secs) { return moment.duration(parseInt(secs), "seconds").humanize(); }
+        });
+
+    // Wait for the DOM to be ready before showing the network and appCache
+    // state.
+    $(function () {
+        // Enable the UI bindings for the network and appCache displays
+        require('./uiNetwork')();
+        require('./uiAppCache')();
+
+        var Story = Backbone.Model.extend({
+          idAttribute : "id",
+          crop : function() {
+            return this.get("image").crop.filter(function(e) { return e.type == "square"; }).pop();
+          }
+        });
+
+        var StoryList = Backbone.Collection.extend({
+          model : Story,
+          comparator : function comparator(story) {
+            return (Date.parse(story.get("pubDate").$text) * -1);
+          },
+          initialize : function () {
+          },
+          url : 'http://localhost:8080/stories',
+          parse: function(response) {
+            if (response && response.list && response.list.story) {
+                return response.list.story;
+            }
+            return null;
+          }
+        });
+
+        var Stories = new StoryList;
+        //window.Stories.sync();
+
+        var StoryListView = Backbone.View.extend({
+          tagName:  "div",
+          className: "stories clearfix",
+          initialize : function () {
+            this.model.bind("reset", this.render, this);
+          },
+          render : function (eventName) {
+            //$(this.el).empty();
+            _.each(this.model.models, function (story) {
+              if ($("#story-" + story.id, this.el).length <= 0) {
+                $(this.el).append(new StoryListItemView({model:story}).render().el);
+              }
+            }, this);
+            //PlayListItems.load();
+            return this;
+          }
+        });
+
+        var StoryListItemView = Backbone.View.extend({
+          tagName : "div",
+          className : "story-item span3",
+          template: _.template($('#story-item-template').html()),
+          initialize : function() {
+            //this.model.bind("change", this.render, this);
+            this.model.bind("reset", this.render, this);
+          },
+          events: {
+            "click" : "view"
+          },
+          view : function(ev) {
+            console.log(ev);
+          },
+          render : function() {
+            $(this.el).html(this.template(this.model.toJSON()));
+            return this;
+          }
+        });
+
+        // Router
+        var AppRouter = Backbone.Router.extend({
+            scrollPosition : null,
+            routes : {
+                "":"list",
+                "story/:id":"getStory"
+            },
+            initialize : function () {
+                Stories.fetch();
+              this.StoryListView = new StoryListView({model:Stories});
+            },
+            list : function () {
+              if ($("div.stories").length <= 0) {
+                $('#content').append(this.StoryListView.render().el);
+              }
+              //$("ul.stories").fadeIn("fast", function() { $win.scrollTop((this.scrollPosition)? $(this.scrollPosition).offset().top : 0 ); });
+            },
+            getStory : function (id) {
+              // TODO: remember scroll position
+              $("ul.stories").hide();
+              this.scrollPosition = "#story-" + id;
+              var story = window.Stories.get(id);
+              if (story == null) {
+                console.log("story == null");
+                StoriesStore.on("ready",
+                              function waitfordb() {
+                                console.log("story == null");
+                                StoriesStore.off("ready", waitfordb, this);
+                                app.getStory(id);
+                                console.log("story == null");
+                              },
+                              this);
+                return;
+              }
+              //console.log(id, window.Stories, story);
+              var view = new window.StoryView({model:story});
+
+              if ($("div.story").length <= 0) {
+                $('#content').append(view.render().el);
+              } else {
+                $("div.story").html(view.render().el)
+              }
+              $("div.story").fadeIn("fast", function() { $win.scrollTop(0); });
+              
+            }
+        });
+
+        var app = new AppRouter();
+        Backbone.history.start();
+
+    });
+});
